25cb2a5996cd1fffd29c3d82e2ef0ed951723065
Clint Losee <clint.losee@rcwilley.com>
2017-10-25 08:43:32 -0600

    Finish setting up survey submittal and call POST endpoint.

--- server/client/src/components/surveys/SurveyForm.js
+++ server/client/src/components/surveys/SurveyForm.js
@@ -5,17 +5,11 @@ import { Link } from 'react-router-dom';
 
 import SurveyField from './SurveyField';
 import validateEmails from '../../utils/validateEmails';
-
-const FIELDS = [
-    { name: 'title', label: 'Survey Title' },
-    { name: 'subject', label: 'Subject Line' },
-    { name: 'body', label: 'Email Body' },
-    { name: 'emails', label: 'Recipient List' }
-];
+import formFields from './formFields';
 
 class SurveyForm extends Component {
     renderFields() {
-        return _.map(FIELDS, ({ label, name }) => {
+        return _.map(formFields, ({ label, name }) => {
             return (
                 <Field
                     key={name}
@@ -32,9 +26,7 @@ class SurveyForm extends Component {
         return (
             <div>
                 <form
-                    onSubmit={this.props.handleSubmit(values =>
-                        console.log(values)
-                    )}
+                    onSubmit={this.props.handleSubmit(this.props.onSurveySubmit)}
                 >
                     {this.renderFields()}
                     <Link to="/surveys" className="red btn-flat white-text">
@@ -43,7 +35,7 @@ class SurveyForm extends Component {
                     </Link>
                     <button type="submit" className="teal btn-flat right white-text">
                         Next
-                        <i className="material-icons right">done</i>
+                        <i className="material-icons right">arrow_forward</i>
                     </button>
                 </form>
             </div>
@@ -54,9 +46,9 @@ class SurveyForm extends Component {
 function validate(values) {
     const errors = {};
 
-    errors.emails = validateEmails(values.emails || '');
+    errors.recipients = validateEmails(values.recipients || '');
 
-    _.each(FIELDS, ({name}) => {
+    _.each(formFields, ({name}) => {
         if (!values[name]) {
             errors[name] = 'You must provide a value';
         }
@@ -67,5 +59,6 @@ function validate(values) {
 
 export default reduxForm({
     validate,
-    form: 'surveyForm'
+    form: 'surveyForm',
+    destroyOnUnmount: false
 })(SurveyForm);
